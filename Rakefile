33ca483a4ca6fce8847392af4f6002d5f43f7721
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2017-07-06 23:13:26 -0400

    update

--- Rakefile
+++ Rakefile
@@ -1,4 +1,6 @@
 task :default do
   sh "../geojson-polygon-labels/bin/geojson-polygon-labels --include-area=true ../geo-countries/data/countries.geojson > polylabel-countries-all.geojson"
   sh "ruby filter.rb"
+  sh "../tippecanoe/tippecanoe -f --output=polylabel-countries.mbtiles --layer=polylabel --minimum-zoom=0 --maximum-zoom=8 --base-zoom=0 polylabel-countries.geojson"
+  sh "ruby fan-out.rb"
 end
--- filter.rb
+++ filter.rb
@@ -8,14 +8,14 @@ data['features'].each {|f|
     maxes[f['properties']['ISO_A3']]
   ].max
 }
+maxes['MYS'] = 131341054
 target = {:type => 'FeatureCollection', :features => []}
 data['features'].each {|f|
   if(maxes[f['properties']['ISO_A3']] == f['properties']['_area'])
-    print 'o'
     f['properties']['ISO_A3'] = 'FRA' if f['properties']['ADMIN'] == 'France'
+    f['properties'].delete('_area')
     target[:features].push(f)
   else
-    print 'x'
   end
 }
 File.write('polylabel-countries.geojson', JSON::dump(target))
